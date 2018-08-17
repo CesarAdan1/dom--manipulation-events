da8ca12a8a1ebf7893849286fa0856751074cdaf
CesarAdan1 <whoclash@hotmail.com>
2018-08-16 19:39:03 -0500

    one

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// GO!
+
+// TASK 1 -- Show/Hide Nav
+    var menu = document.querySelector('.nav.menu');
+
+    selection.forEach(function(show){
+        hide.addEventListener('click', function(event){
+            var shower = event.target;
+            
+            
+        })
+
+    })
+
+// TASK 2 -- Select an Icon
+
+
+var options = document.querySelectorAll('.wish-list div');
+
+options.forEach(function(option){
+    
+    option.addEventListener('click', function(event){
+        var optional = event.currentTarget;
+        optional.classList.toggle('selected'); 
+    })
+})
+
+    
+// TASK 3 -- Move Item From List to List
+
+
+// TASK 4 -- Add Guest to List
+
+// TASK 5 -- (Adventure Mode)-- Add + Remove Item From List
